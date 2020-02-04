6f5b4f11bdd264e3d8b67ca06b3ec22a18bab349
Peter King <pmpk20@bath.ac.uk>
2020-02-04 17:54:43 +0000

    Translated preprocessing in R to Python

--- Pylot.py
+++ Pylot.py
@@ -16,6 +16,7 @@ import statsmodels.formula.api as smf
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import itertools
 from cycler import cycler
 mpl.rcParams['axes.prop_cycle'] = cycler(color='bgcrmyk')
 
@@ -27,69 +28,39 @@ Pilot = pd.read_csv("\\\\myfiles\pmpk20\dos\PhD\Data Collection\Pilot\PhD Survey
 #####################################################################################
 ################ Section 2: Data pre-processing  ##########################
 #####################################################################################
+
+Pilot = Pilot.drop([Pilot.columns[0],Pilot.columns[1],Pilot.columns[7],Pilot.columns[26]],axis='columns')
+Pilot = Pilot.reset_index()
+
+Pilot.columns = ["ID","Q1Gender", "Q2Age", "Q3Distance", "Q4Trips","Q5CVM1","Q6QOV","Q7CE1", "Q8CE2","Q9CE3","Q10Action", "Q11Self","Q12Others", "Q13Marine", "Q14BP","Q15Responsibility","Q16Charity", "Q17Understanding", "Q18Consequentiality", "Q19Experts", "Q20Education","Q21Employment", "Q22Income","Q23Survey"]
+
+Pilot2 = Pilot
+
+SpecificChoices = pd.DataFrame({'Effectiveness.ALT':[0.0,0.0,0.0],'Env.ALT':[90.0,40.0,40.0],'Price.ALT':[0.0,1.0,1.0],'Health.ALT':[0.0,0.1,0.6] })
+
+SQChoices = pd.DataFrame({'Effectiveness.SQ':[0.0,0.0,0.0],'Env.SQ':[0.0,0.0,0.0],'Price.SQ':[0.0,0.0,0.0],'Health.SQ':[1.0,1.0,1.0] })
+
+Pilot2 = Pilot2.loc[Pilot2.index.repeat(SpecificChoices.shape[0])]
+SpecificChoices = SpecificChoices.loc[SpecificChoices.index.repeat(Pilot.shape[0])]
+SQChoices= SQChoices.loc[SQChoices.index.repeat(Pilot.shape[0])]
+SpecificChoices.index = Pilot2.index
+SQChoices.index = Pilot2.index
+Test = pd.concat([Pilot,SpecificChoices,SQChoices],axis=1)
+
+Choices = pd.concat([Pilot.iloc[:,7], Pilot.iloc[:,8], Pilot.iloc[:,9]],axis=1).transpose()
+Choices = Choices.unstack().reset_index(drop=True)
+Test['Q7CE1'] = Choices
+Test = Test.drop([Test.columns[8],Test.columns[9]],axis='columns')
+Task = pd.DataFrame(itertools.chain.from_iterable(itertools.repeat(range(3), Pilot.shape[0])))
+Task.index = Test.index
+Test.insert(1,"Task",Task,True)
+Task = pd.DataFrame(itertools.chain.from_iterable(itertools.repeat(range(3), Pilot.shape[0])))
+
+Test.columns = ["ID","Task","Q1Gender", "Q2Age", "Q3Distance", "Q4Trips","Q5CVM1","Q6QOV","Choice","Q10Action", "Q11Self","Q12Others", "Q13Marine", "Q14BP","Q15Responsibility","Q16Charity", "Q17Understanding", "Q18Consequentiality", "Q19Experts", "Q20Education","Q21Employment", "Q22Income","Q23Survey","Effectiveness.ALT","Env.ALT","Price.ALT","Health.ALT","Effectiveness.SQ","Env.SQ","Price.SQ","Health.SQ"]
+
+#Tests <- data.frame(Test[,1:2],Test$Choice,Test[,24:31])
+## Takes the core elements of the TEST data frame
 #
 #
-#Pilot <- Pilot[ -c(1,2,8,27)] #Drop rows of no importance to the quantitative analysis, namely text responses.
-#
-#colnames(Pilot) <- c("Q1Gender", "Q2Age", "Q3Distance", "Q4Trips","Q5CVM1","Q6QOV","Q7CE1", "Q8CE2","Q9CE3","Q10Action", "Q11Self","Q12Others", "Q13Marine", "Q14BP","Q15Responsibility","Q16Charity", "Q17Understanding", "Q18Consequentiality", "Q19Experts", "Q20Education","Q21Employment", "Q22Income","Q23Survey") #Renames columns for ease of analysis
-#
-#Pilot2 <- Pilot # Create a backup of the Pilot data
-#
-#Pilot2 <- data.frame(Pilot2) # Force to the data.frame format
-#
-#SpecificChoices <- data.frame("Effectiveness.ALT" =c(100,0,0), 
-#                              "Env.ALT" =c(100,90,40),
-#                              "Price.ALT" =c(0,0,1),
-#                              "Health.ALT" =c(0,0.1, 0.1))
-### Aim here is to make a dataframe with the choice set data. Extremely important for later.
-#
-#Effectiveness <- factor(SpecificChoices$Effectiveness.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-#
-#Accumulation <- factor(SpecificChoices$Env.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-#
-#Price <- factor(SpecificChoices$Price.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-#
-#Health <- factor(SpecificChoices$Health.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-#
-#contrasts(Effectiveness) <- contr.sum(unique(SpecificChoices$Effectiveness.ALT)) # Ensures a DCE attribute is appropriately stored as a factor
-#
-#contrasts(Accumulation) <- contr.sum(unique(SpecificChoices$Env.ALT))# Ensures a DCE attribute is appropriately stored as a factor
-#
-#contrasts(Price) <- contr.sum(unique(SpecificChoices$Price.ALT))# Ensures a DCE attribute is appropriately stored as a factor
-#
-#contrasts(Health) <- contr.sum(unique(SpecificChoices$Health.ALT))# Ensures a DCE attribute is appropriately stored as a factor
-#
-#SpecificChoices <- data.frame(Effectiveness, Accumulation, Price, Health) # Creates a dataframe with all the DCE attributes in levels
-#
-#Pilot2$ID <- seq.int(nrow(Pilot2)) # Adds an ID column to the Pilot survey. This is anonymous and bears no relation to actual respondents.
-#
-#for (i in colnames(Pilot2)){
-#  if (is.factor(Pilot[[i]]) == TRUE){
-#    contrasts(Pilot2[,i]) <- contr.sum(unique(Pilot2[,i]))
-#  }
-#}
-### Aim of the function is to express all variables in the Pilot data as factors
-#
-#library(dplyr) # Important for data manipulation
-#
-#Test <- cbind(slice(.data = Pilot2,rep(1:n(), each = nrow(SpecificChoices))),slice(.data = SpecificChoices,rep(1:n(), times = nrow(Pilot2))))
-## So TEST is a dataframe that transforms the PILOT data into an appropriate format for the estimation.
-## The code repeats each row of the PILOT data for each choice the respondent made. Therefore, each respondent now has three rows one for Q7, Q8, Q9.
-#
-#Choices <- data.frame(a = c(t(data.frame(rep(Pilot2[,7:9], times=1)))[,]))
-## Forces the responses to Q7, Q8, Q9 from PILOT to be in one long column. Each respondent has three rows.
-#
-#Test <- data.frame(Test$ID,Test[1:6],Choices,Test[10:23],Test[25:28])
-## Combines and reorders the TEST dataframe. Use View(Test) to see that this dataframe combines the choice sets, responses, and respondent data.
-#
-#colnames(Test) <- c("ID","Q1Gender", "Q2Age", "Q3Distance", "Q4Trips","Q5CVM1","Q6QOV","Choice","Q10Action", "Q11Self","Q12Others", "Q13Marine", "Q14BP","Q15Responsibility","Q16Charity", "Q17Understanding", "Q18Consequentiality", "Q19Experts", "Q20Education","Q21Employment", "Q22Income","Q23Survey","Effectiveness","Accumulation","Price","Health")
-## Adds and updates column names.
-#
-#
-###########################################################################
-################ Section 3: Estimation of models ##########################
-###########################################################################
-#
-#
-#library(nnet)
-#summary(multinom(Test$Choice ~ Test$Q1Gender +Test$Q2Age +Test$Q3Distance+Test$Q4Trips+Test$Q10Action+Test$Q11Self + Test$Q12Others + Test$Q13Marine + Test$Q14BP + Test$Q15Responsibility + Test$Q16Charity + Test$Q17Understanding + Test$Q18Consequentiality + Test$Q19Experts + Test$Q20Education + Test$Q21Employment + Test$Q22Income + Test$Q23Survey, data=Test))
+#levels(Tests$Test.Choice)[1] <- "ALT"
+#levels(Tests$Test.Choice)[2] <- "SQ"
