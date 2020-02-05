e3043397af03627300a2367b9a1536bec6f3f6cf
Peter King <pmpk20@bath.ac.uk>
2020-02-05 17:56:56 +0000

    05/02 progress on the R script estimating more models and with a MNL

--- PilotAnalysis.R
+++ PilotAnalysis.R
@@ -15,10 +15,11 @@
 
 
 install.packages("dplyr") # Useful later for data manipulation
+install.packages("apollo")
 install.packages("nnet") # Helps estimation of the MNL baseline model
 install.packages("AER")
 install.packages("mlogit")
-
+rm(list = ls())
 ############ Importing data:
 
 setwd("H:/PhDPilotSurvey") # Sets working directory. This is where my Github repo is cloned to.
@@ -39,6 +40,42 @@ Pilot2 <- Pilot # Create a backup of the Pilot data
 
 Pilot2 <- data.frame(Pilot2) # Force to the data.frame format
 
+for (i in colnames(Pilot)){
+  if (is.factor(Pilot[[i]]) == TRUE){
+    Pilot2[[i]] <- as.numeric(Pilot[[i]])-1
+  }
+}
+
+Pilot2$Q3Distance[Pilot2$Q3Distance == 1] <- 4
+Pilot2$Q3Distance[Pilot2$Q3Distance == 0] <- 1
+Pilot2$Q6QOV <- t(t(1-Pilot2$Q6QOV)) # Change the QOV coding so 1 is precaution
+Pilot2$Q7CE1[Pilot2$Q7CE1 == 0] <-2 #The CE sets Status Quo as 1 and Alternative to 0 so this switches it around
+Pilot2$Q7CE1[Pilot2$Q7CE1 == 1] <-0
+Pilot2$Q7CE1[Pilot2$Q7CE1 == 2] <-1
+Pilot2$Q8CE2[Pilot2$Q8CE2 == 0] <-2
+Pilot2$Q8CE2[Pilot2$Q8CE2 == 1] <-0
+Pilot2$Q8CE2[Pilot2$Q8CE2 == 2] <-1
+Pilot2$Q9CE3[Pilot2$Q9CE3 == 0] <-2
+Pilot2$Q9CE3[Pilot2$Q9CE3 == 1] <-0
+Pilot2$Q9CE3[Pilot2$Q9CE3 == 2] <-1
+Pilot2$Q17Understanding[Pilot2$Q17Understanding == 0] <-3 # Understanding question should be weak, average, strong not 1,2,0
+Pilot2$Q20Education[Pilot2$Q20Education == 2] <-3 #Just shifting every value up one for education
+Pilot2$Q20Education[Pilot2$Q20Education == 1] <-2
+Pilot2$Q20Education[Pilot2$Q20Education == 0] <-1
+Pilot2$Q21Employment[Pilot2$Q21Employment == 0] <-5 #Switch full to 5 for now
+Pilot2$Q21Employment[Pilot2$Q21Employment == 1] <-0 #Change NEET to zero
+Pilot2$Q21Employment[Pilot2$Q21Employment == 2] <-1 #Change Part to 1
+Pilot2$Q21Employment[Pilot2$Q21Employment == 4] <-2 #Self stays same so student changes to 2
+Pilot2$Q21Employment[Pilot2$Q21Employment == 5] <-4 #Put full back as highest value
+Pilot2$Q22Income[Pilot2$Q22Income == 7] <-0.5 #The only wrong assignment of Employment was the 500-1000 level which it put last?
+Pilot2$Q22Income[Pilot2$Q22Income == 6] <-7
+Pilot2$Q22Income[Pilot2$Q22Income == 5] <-6
+Pilot2$Q22Income[Pilot2$Q22Income == 4] <-5
+Pilot2$Q22Income[Pilot2$Q22Income == 3] <-4
+Pilot2$Q22Income[Pilot2$Q22Income == 2] <-3
+Pilot2$Q22Income[Pilot2$Q22Income == 1] <-2
+Pilot2$Q22Income[Pilot2$Q22Income == 0.5] <-1
+
 SpecificChoices <- data.frame("Effectiveness.ALT" =c(0,0,0), 
                               "Env.ALT" =c(90,40,40),
                               "Price.ALT" =c(0,1,1),
@@ -49,24 +86,6 @@ SQChoices <- data.frame("Effectiveness.SQ" =c(0,0,0),
                               "Price.SQ" =c(0,0,0),
                               "Health.SQ" =c(1,1, 1))
 
-## Aim here is to make a dataframe with the choice set data. Extremely important for later.
-
-Effectiveness <- factor(SpecificChoices$Effectiveness.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-
-Accumulation <- factor(SpecificChoices$Env.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-
-Price <- factor(SpecificChoices$Price.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-
-Health <- factor(SpecificChoices$Health.ALT) #Forces an attribute of the DCE to be a factor for ease of analysis
-
-contrasts(Effectiveness) <- contr.sum(unique(SpecificChoices$Effectiveness.ALT)) # Ensures a DCE attribute is appropriately stored as a factor
-
-contrasts(Accumulation) <- contr.sum(unique(SpecificChoices$Env.ALT))# Ensures a DCE attribute is appropriately stored as a factor
-
-contrasts(Price) <- contr.sum(unique(SpecificChoices$Price.ALT))# Ensures a DCE attribute is appropriately stored as a factor
-
-contrasts(Health) <- contr.sum(unique(SpecificChoices$Health.ALT))# Ensures a DCE attribute is appropriately stored as a factor
-
 SpecificChoices <- data.frame(Effectiveness, Accumulation, Price, Health) # Creates a dataframe with all the DCE attributes in levels
 
 
@@ -74,9 +93,11 @@ Pilot2$ID <- seq.int(nrow(Pilot2)) # Adds an ID column to the Pilot survey. This
 
 for (i in colnames(Pilot2)){
   if (is.factor(Pilot[[i]]) == TRUE){
-    contrasts(Pilot2[,i]) <- contr.sum(unique(Pilot2[,i]))
+    contrasts(Pilot2[,i]) <- contr.sum(nlevels(Pilot2[,i]))
   }
 }
+
+
 ## Aim of the function is to express all variables in the Pilot data as factors
 
 library(dplyr) # Important for data manipulation
@@ -100,36 +121,124 @@ Tests <- data.frame(Test[,1:2],Test$Choice,Test[,24:31])
 
 levels(Tests$Test.Choice)[1] <- "ALT"
 levels(Tests$Test.Choice)[2] <- "SQ"
+Test$av_ALT <- rep(1,nrow(Test))
+Test$av_SQ <- rep(1,nrow(Test))
+
+##########################################################################
+############### Section 3: Estimation of DCE models #####################
+##########################################################################
+
+# Here I try and adapt a MNL example from the APOLLO package: https://cran.r-project.org/web/packages/apollo/vignettes/apollofirstexample.html
+## Package documentation: https://cran.r-project.org/web/packages/apollo/apollo.pdf
+
+library(apollo)
+apollo_initialise()
+apollo_control = list(
+  modelName  ="MNL",
+  modelDescr ="Simple MNL model on mode choice SP data",
+  indivID    ="ID"
+)
+database <- Test
+database$mean_income <- mean(Test$Q22Income)
+
+apollo_beta=c(asc_ALT  = 0,
+              asc_SQ  = 0,
+              b_tt_ALT = 0,
+              b_tt_SQ = 0,
+              b_c      = 0)
+apollo_fixed = c("asc_SQ")
+apollo_inputs = apollo_validateInputs()
+
+apollo_probabilities=function(apollo_beta, apollo_inputs, 
+                              functionality="estimate"){
+  
+  ### Attach inputs and detach after function exit
+  apollo_attach(apollo_beta, apollo_inputs)
+  on.exit(apollo_detach(apollo_beta, apollo_inputs))
+  
+  ### Create list of probabilities P
+  P = list()
+  
+  ### List of utilities: these must use the same names as
+  ### in mnl_settings, order is irrelevant.
+  V = list()
+  V[['ALT']] = asc_ALT + b_tt_ALT *Health_ALT + b_c*Price_ALT
+  V[['SQ']] = asc_SQ + b_tt_SQ *Health_SQ + b_c*Price_SQ
+  ### Define settings for MNL model component
+  mnl_settings = list(
+    alternatives  = c(ALT=1, SQ=0), 
+    avail         = list(ALT=av_ALT, SQ=av_SQ), 
+    choiceVar     = Choice,
+    V             = V
+  )
+  
+  ### Compute probabilities using MNL model
+  P[['model']] = apollo_mnl(mnl_settings, functionality)
+  
+  ### Take product across observation for same individual
+  P = apollo_panelProd(P, apollo_inputs, functionality)
+  
+  ### Prepare and return outputs of function
+  P = apollo_prepareProb(P, apollo_inputs, functionality)
+  return(P)
+}
+
+
+model = apollo_estimate(apollo_beta, apollo_fixed, 
+                        apollo_probabilities, 
+                        apollo_inputs)
+apollo_modelOutput(model)
+apollo_saveOutput(model)
+
+predictions_base = apollo_prediction(model, 
+                                     apollo_probabilities, 
+                                     apollo_inputs)
+
+database$cost_rail = 1.1*database$cost_rail
+predictions_new = apollo_prediction(model, 
+                                    apollo_probabilities, 
+                                    apollo_inputs)
+
+change=(predictions_new-predictions_base)/predictions_base
+
+change=change[,-ncol(change)]
+
+summary(change)
 
 
 ##########################################################################
-############### Section 3: Estimation of DCE models ##########################
+############### Section 3B: Estimation of CVM models #####################
 ##########################################################################
 
-##MULTINOM
-library(nnet)
-summary(multinom(Test$Choice ~ Test$Q1Gender +Test$Q2Age +Test$Q3Distance+Test$Q4Trips+Test$Q10Action+Test$Q11Self + Test$Q12Others + Test$Q13Marine + Test$Q14BP + Test$Q15Responsibility + Test$Q16Charity + Test$Q17Understanding + Test$Q18Consequentiality + Test$Q19Experts + Test$Q20Education + Test$Q21Employment + Test$Q22Income + Test$Q23Survey, data=Test))
 
-##MLOGIT
+summary(glm(Test$Q5CVM1 ~ Test$Q1Gender + Test$Q2Age + Test$Q3Distance + Test$Q4Trips + Test$Q10Action + Test$Q22Income + Test$Q21Employment + Test$Q20Education + Test$Q11Self + Test$Q12Others + Test$Q13Marine + Test$Q14BP + Test$Q15Responsibility + Test$Q16Charity + Test$Q17Understanding, data=Test))
 
-
-library(dplyr)
-library(AER)
-library(mlogit)
-
-TM <- mlogit.data(Test, choice = "Choice", shape = "long", chid.var = "ID", alt.var = "Task", drop.index = FALSE,varying = 24:27)
-View(TM)
-summary(mlogit(Choice ~ TM$Q10Action + TM$Q11Self + TM$Q12Others, TM,reflevel = "3"))
-
-TM <- mlogit.data(Test, shape = "wide", choice="Choice",
-                  varying = 24:31,sep="_",id.var = "ID")
+# Here I've made a function that plots which variables are the most significant
+Significance <- function(Model) {
+  PV <- data.frame(summary(Model)$coefficients[,4],summary(Model)$coefficients[,1] )
+  colnames(PV) <- c("PV","Effect")
+  PV <- subset(PV,PV <=0.05)
+  PV <- data.frame(row.names(PV), PV$PV, PV$Effect)
+  colnames(PV) <- c("Variables","PV","Effect")
+  return(barplot(PV$Effect, names.arg = PV$Variables,xlab = "Variables",ylab = "Effect",ylim = c(-2,5),axes = TRUE))
+}
 
 ##########################################################################
-############### Section 3B: Estimation of CVM models ##########################
+############### Section 3C: Estimation of QOV models #####################
 ##########################################################################
-levels(TM$Q6QOV)[1] <- 1
-levels(TM$Q6QOV)[2] <- 0
 
+
+require(ggplot2)
+ModelQOV <- (glm(Test$Q6QOV ~ Test$Q1Gender + Test$Q2Age + Test$Q3Distance + Test$Q4Trips + Test$Q10Action + Test$Q22Income + Test$Q21Employment + Test$Q20Education + Test$Q11Self + Test$Q12Others + Test$Q13Marine + Test$Q14BP + Test$Q15Responsibility + Test$Q16Charity + Test$Q17Understanding, data=Test))
+Significance(ModelQOV)
+ggplot(Test, aes(x = Q6QOV, y = Q3Distance, colour = Health_SQ)) + geom_point() + facet_wrap(~Q1Gender)
+
+
+##########################################################################
+############### Section 4: Estimation of other models #####################
+##########################################################################
+
+
+# Testing determinants of belief in experts
 Model1 <- lm(Test$Q19Experts ~ Test$Q1Gender + Test$Q2Age + Test$Q3Distance + Test$Q4Trips + Test$Q10Action + Test$Q22Income + Test$Q21Employment + Test$Q20Education + Test$Q11Self + Test$Q12Others + Test$Q13Marine + Test$Q14BP + Test$Q15Responsibility + Test$Q16Charity + Test$Q17Understanding)
-
-
+Significance(Model1)
