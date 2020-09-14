7a7440d53ffa1028565e3e28ae1ff21b3de0dcd3
Peter King <pmpk20@bath.ac.uk>
2020-09-14 21:04:02 +0100

    14/09: Started spatial mapping of WTP with no luck.

--- FullAnalysis.R
+++ FullAnalysis.R
@@ -2,10 +2,9 @@
 #### Introduction: Full survey data analysis script  ###############
 
 
-#### 31/07 To Do: ####
-#### - Estimate APOLLO MXL PMC, MLHS, Sobol
-#### - Estimate CE MFX
+#### 14/09 To Do: ####
 #### - Fix CV ICLV
+#### - Spatial WTP map
 
 
 #### Section 0: Package installation ####
@@ -3141,6 +3140,47 @@ KnowledgeGraphD <- ggplot(Full_Final, aes(x=Q2Age)) +
   scale_y_continuous(name="Likert scale of knowledge",breaks = waiver(), n.breaks=5,
                      limits=c(1,5))+
   labs(x = "Age",y="Concern")
+
+
+
+
+library(maps)
+library(mapdata)
+library(maptools)
+library(rgdal)
+library(ggmap)
+library(ggplot2)
+library(rgeos)
+library(broom)
+library(plyr)
+
+#Load the shapefile - make sure you change the filepath to where you saved the shapefiles
+shapefile <- readOGR("NUTS_Level_2__January_2018__Boundaries.shp")
+
+#Reshape for ggplot2 using the Broom package
+mapdata <- tidy(shapefile) #This might take a few minutes
+
+#Check the shapefile has loaded correctly by plotting an outline map of the UK
+gg <- ggplot() + geom_polygon(data = mapdata, aes(x = long, y = lat, group = group), color = "#FFFFFF", size = 0.25)
+gg <- gg + coord_fixed(1) #This gives the map a 1:1 aspect ratio to prevent the map from appearing squashed
+print(gg)
+
+#Create some data to use in the heatmap - here we are creating a random "value" for each county (by id)
+mydata <- data.frame(id=unique(mapdata$id), value=sample(c(0:100), length(unique(mapdata$id)), replace = TRUE))
+
+#Join mydata with mapdata
+df <- join(mapdata, mydata, by="id")
+
+#Create the heatmap using the ggplot2 package
+gg <- ggplot() + geom_polygon(data = df, aes(x = long, y = lat, group = group, fill = value), color = "#FFFFFF", size = 0.25)
+gg <- gg + scale_fill_gradient2(low = "blue", mid = "red", high = "yellow", na.value = "white")
+gg <- gg + coord_fixed(1)
+gg <- gg + theme_minimal()
+gg <- gg + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = 'none')
+gg <- gg + theme(axis.title.x=element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
+gg <- gg + theme(axis.title.y=element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+print(gg)
+
 
 
 
